211c78b2823123333e9d87721e5096cdd75c6598
“Marc <marc_piantone@live.fr>
2020-07-27 15:23:28 -0400

    Adjustments

--- src/hooks/usePersistedState.hook.js
+++ src/hooks/usePersistedState.hook.js
@@ -1,14 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const usePersistedState = (key, value) => {
+  //Fetch and turn to number / json
+  const localValue = JSON.parse(localStorage.getItem(key));
+  //Return if it exists
+  const currentLocalValue = localValue ? localValue : value;
+
   //Declare new state :
-  const [currentValue, setCurrentValue] = useState(value);
+  const [currentValue, setCurrentValue] = useState(currentLocalValue);
+
   //Store in localstorage this new value using state :
-  localStorage.setItem(key, JSON.stringify(currentValue));
-  // Fetch and turn to number / json
-  const currentLocalValue = JSON.parse(localStorage.getItem(key));
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(currentValue));
+  }, [currentValue]);
+
   //return for use
-  return [currentLocalValue, setCurrentValue];
+  return [currentValue, setCurrentValue];
 };
 
 export default usePersistedState;
