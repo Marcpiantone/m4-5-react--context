b993e6f0449060f19fff8853d811522f7e644f47
“Marc <marc_piantone@live.fr>
2020-07-27 20:35:14 -0400

    Answer exercise-4 cookies while asleep

--- src/components/GameProvider.js
+++ src/components/GameProvider.js
@@ -5,7 +5,13 @@ import usePersistedState from "../hooks/usePersistedState.hook";
 import items from "../data";
 
 export const GameProvider = ({ children }) => {
-  const [numCookies, setNumCookies] = usePersistedState("num-cookies", 1000);
+  const [numCookies, setNumCookies, timeDiff] = usePersistedState(
+    "num-cookies",
+    1000
+  );
+
+  console.log(`Time elapsed ${Math.round(timeDiff / 1000)} sec`);
+
   const [purchasedItems, setPurchasedItems] = usePersistedState("items", {
     cursor: 0,
     grandma: 0,
@@ -22,7 +28,7 @@ export const GameProvider = ({ children }) => {
       const item = items.find((item) => item.id === itemId);
       const value = item.value;
 
-      return acc + value * numOwned;
+      return (acc + value * numOwned) * Math.round(timeDiff / 1000);
     }, 0);
   };
 
--- src/hooks/usePersistedState.hook.js
+++ src/hooks/usePersistedState.hook.js
@@ -3,22 +3,29 @@ import { useState, useEffect } from "react";
 const usePersistedState = (key, value) => {
   //Fetch and turn to number / json
   const localValue = JSON.parse(localStorage.getItem(key));
-  //Return if it exists
-  const currentLocalValue = localValue ? localValue : value;
+
+  //Return localeValue if it exists
+  const currentLocalValue = localValue ? localValue.currentValue : value;
+
+  //Retrieve timestamp for use if exists
+  const timeDiff = localValue ? new Date().getTime() - localValue.timestamp : 0;
 
   //Declare new state :
   const [currentValue, setCurrentValue] = useState(currentLocalValue);
 
+  //Add a timestamp for the localstorage
+  const objectStored = {
+    currentValue: currentValue,
+    timestamp: new Date().getTime(),
+  };
+
   //Store in localstorage this new value using state :
-  useEffect(
-    (key) => {
-      localStorage.setItem(key, JSON.stringify(currentValue));
-    },
-    [currentValue]
-  );
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(objectStored));
+  }, [currentValue]);
 
   //return for use
-  return [currentValue, setCurrentValue];
+  return [currentValue, setCurrentValue, timeDiff];
 };
 
 export default usePersistedState;
